d3a53b66b579351df34436f90d6876661ada79de
Wozzy1 <134657893+Wozzy1@users.noreply.github.com>
2023-06-10 15:37:11 -0500

    Add files via upload

--- FFA KMean.py
+++ FFA KMean.py
@@ -1,19 +1,26 @@
 from sklearn.cluster import KMeans
 import numpy as np
-
 import pandas
 
 df = pandas.read_csv("C:/Users/diepw/Downloads/Ag Ed for All - Groupings - asd.csv", 
                          usecols = ['Name', 'Lat & Long'])
 
-print(df)
+#print(df)
+
+coords_ = df['Lat & Long'].tolist()
+coords = [(float(lat), float(lon)) for lat, lon in coords_]
+
 quit()
+kmeans = KMeans(n_clusters=24, random_state=0).fit(coords_)
+
+cluster_labels = kmeans.labels_
+
+for i, label in enumerate(cluster_labels):
+    print(f"Point {i+1}: Cluster {label+1}")
+
+"""
 # Define the latitude and longitude points
-points = np.array([
-    [40.412483, -91.392114],
-    [40.577006, -91.518805],
-    # ... (add the remaining points here)
-])
+points = np.array()
 
 # Perform k-means clustering with 24 clusters
 kmeans = KMeans(n_clusters=24, random_state=0).fit(points)
@@ -23,4 +30,4 @@ cluster_labels = kmeans.labels_
 
 # Print the cluster labels for each point
 for i, label in enumerate(cluster_labels):
-    print(f"Point {i+1}: Cluster {label+1}")
+    print(f"Point {i+1}: Cluster {label+1}")"""
